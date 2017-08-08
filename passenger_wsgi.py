5e8ced59f64e9c5f328f828b0e2fa9cd2402de2f
djyoshabyd <contact@dusette.net>
2017-08-08 15:56:55 -0700

    prepping to deploy

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import sys, os
+INTERP = "/home/djyoshabyd/Django/envs/DusetteBlog/bin/python"
+#INTERP is present twice so that the new Python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append(cwd + '/blog.dusette.net')  #You must add your project here
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "HeavyCrown.settings_production"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
